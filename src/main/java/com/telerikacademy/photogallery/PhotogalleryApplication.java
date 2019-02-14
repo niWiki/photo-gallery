f78ffb7bf920b35f8b00963162e318009eae72d3
nadya <nadya@telerikacademy.com>
2019-02-14 17:08:06 +0200

    add intellij proj

---
+++ src/main/java/com/telerikacademy/photogallery/PhotogalleryApplication.java
@@ -0,0 +1,14 @@
+package com.telerikacademy.photogallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotogalleryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotogalleryApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/telerikacademy/photogallery/PhotogalleryApplicationTests.java
@@ -0,0 +1,17 @@
+package com.telerikacademy.photogallery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotogalleryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
