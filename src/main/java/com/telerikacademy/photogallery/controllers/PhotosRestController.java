1048fb19f1e51abb4d7b0db1ce3ae3c78e3f07a3
nadya <nadya@telerikacademy.com>
2019-02-14 17:32:01 +0200

    annotate controller

---
+++ src/main/java/com/telerikacademy/photogallery/controllers/PhotosRestController.java
@@ -0,0 +1,10 @@
+package com.telerikacademy.photogallery.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/photos")
+public class PhotosRestController {
+
+}
