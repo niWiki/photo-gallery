ba9d1e7355f64ee9ab434f1725a1647f1115e17a
Nadya Atanasova <nadya@telerikacademy.com>
2019-05-27 11:42:59 +0300

    Rename route

--- src/main/java/com/telerikacademy/photogallery/controllers/FavouritesRestController.java
+++ src/main/java/com/telerikacademy/photogallery/controllers/FavouritesRestController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/favourites")
+@RequestMapping("/api/favs")
 public class FavouritesRestController {
 }
