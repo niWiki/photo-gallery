a1469583dcbc8d8c7bc51fe6a2f6ec9c3186cf76
Nadya Atanasova <nadya@telerikacademy.com>
2019-05-27 11:51:05 +0300

    Add getting favs

--- src/main/java/com/telerikacademy/photogallery/controllers/FavouritesRestController.java
+++ src/main/java/com/telerikacademy/photogallery/controllers/FavouritesRestController.java
@@ -1,9 +1,19 @@
 package com.telerikacademy.photogallery.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/favourites")
 public class FavouritesRestController {
+    List<String> photoUrls = new ArrayList<>();
+
+    @GetMapping
+    public List<String> getPhotoUrls() {
+        return new ArrayList<>(photoUrls);
+    }
 }
