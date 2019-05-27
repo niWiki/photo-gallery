b5cc7395c2cd018d8cb5886505fb1232156cbf87
Nadya Atanasova <nadya@telerikacademy.com>
2019-05-27 10:47:25 +0300

    Add Favs controller

---
+++ src/main/java/com/telerikacademy/photogallery/controllers/FavouritesRestController.java
@@ -0,0 +1,9 @@
+package com.telerikacademy.photogallery.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/favourites")
+public class FavouritesRestController {
+}
