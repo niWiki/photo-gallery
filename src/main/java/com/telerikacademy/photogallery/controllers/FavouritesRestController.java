4342197f6d88972e733d124e7cea1a33297b2f92
niWiki <39672514+niWiki@users.noreply.github.com>
2019-08-22 15:34:07 +0300

    Update FavouritesRestController.java

--- src/main/java/com/telerikacademy/photogallery/controllers/FavouritesRestController.java
+++ src/main/java/com/telerikacademy/photogallery/controllers/FavouritesRestController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RequestMapping("/api/favourites")
 public class FavouritesRestController {
     List<String> photoUrls = new ArrayList<>();
+    int pages = 10;
 
     @GetMapping
     public List<String> getPhotoUrls() {
